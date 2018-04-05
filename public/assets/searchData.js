004f9bd280cf6269b1ce413c22ac84675d5637e4
Dagfinn Reitan <dagfinn.reitan@novacare.no>
2018-04-05 10:32:36 +0200

    added typed.js

---
+++ public/assets/searchData.js
@@ -0,0 +1,68 @@
+const data = [
+    {
+      id: 1,
+      categoryTitle: 'Feriepenger',
+      searchableContent: '',
+      posts: [
+        {
+          title: 'Feriepenger',
+          url: '/tema-side',
+          searchableContent: '',
+        },
+      ],
+    },
+    {
+      id: 2,
+      categoryTitle: 'Ledelse og lederrollen',
+      searchableContent: '',
+      posts: [
+        {
+          title: 'Bygge tillit og kultur',
+          url: '/tema-side',
+          searchableContent: '',
+        },
+        {
+          title: 'Dette er god ledelse',
+          url: '/tema-side',
+          searchableContent: '',
+        },
+        {
+          title: 'Lederens årshjul - lederoppgaver',
+          url: '/tema-side',
+          searchableContent: '',
+        },
+      ],
+    },
+    {
+      id: 3,
+      categoryTitle: 'Arbeidstid',
+      searchableContent: '',
+      posts: [
+        {
+          title: 'Arbeidstid og overtid deltid, fleksitid, turnus',
+          url: '/tema-side',
+          searchableContent: '',
+        },
+        {
+          title: 'Helligdager, religiøse høytider, kvelds- og nattjobbing, ugunstig arbeidstid',
+          url: '/tema-side',
+          searchableContent: '',
+        },
+        {
+          title: 'Fri og uavhengig stilling',
+          url: '/tema-side',
+          searchableContent: '',
+        },
+        {
+          title: 'Redusert arbeidstid, fri- og uavhengig stilling',
+          url: '/tema-side',
+          searchableContent: '',
+        },
+        {
+          title: 'Overtid og avspasering',
+          url: '/tema-side',
+          searchableContent: '',
+        },
+      ],
+    },
+  ];
--- resources/js/main.js
+++ resources/js/main.js
@@ -5,9 +5,11 @@ import './toggle'
 import $ from 'jquery';
 import {TweenMax, Power2, TimelineLite} from "gsap";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
+import Typed from 'typed.js';
 
 $(".menu__search-input").one( "click", function() {
   TweenMax.to($("#search"), .2, {autoAlpha:1, opacity:1, ease: Circ.easeOut, onComplete: focusSearch});
+  typed = null;
 });
 
 function focusSearch(){
@@ -29,3 +31,35 @@ $("#search__form").submit(function(event) {
     $("#results").html(content);
   });
  });
+
+ // Autocomplete
+ $("#search__form input").on('keyup', function(event) {
+   var val = this.value;
+   if (val.length > 0){
+    TweenMax.set($("#results"), {opacity:0, autoAlpha:0, y:20});
+
+    var $form = $(this),
+      url = $form.attr("action");
+  
+    $.get(url, {q: val}).done(function(data) {
+      var content = data;
+      TweenMax.to($("#results"), .5, {autoAlpha:1, y:0, opacity:1, ease: Circ.easeOut});
+      $("#results").html(content);
+    });
+   } else {
+    console.log("fail", this);
+   }
+ });
+
+// Typed.js
+var options = {
+  strings: [
+    "Søk etter ferie ...",
+    "Søk etter ansettelse ...",
+    "Søk etter ledelse ..."
+  ],
+  typeSpeed: 40,
+  loop: true,
+  attr: 'placeholder',
+};
+new Typed("#search__field", options);
